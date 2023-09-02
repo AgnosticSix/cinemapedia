288dd0eb5263532d1d6adf924eae09840afd650d
MATEO JAVIER AGUILAR CARRILLO <mateo.aguilar@elektra.com.mx>
2023-09-01 23:23:06 -0600

    repository impl and a movie provider added

--- lib/config/infrastructure/datasources/moviedb_datasource.dart
+++ lib/config/infrastructure/datasources/moviedb_datasource.dart
@@ -1,4 +1,6 @@
 import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/config/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/config/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:dio/dio.dart';
@@ -13,10 +15,14 @@ class MoviedbDataSource extends MoviesDatasource {
   ));
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-
     final response = await dio.get('/movie/now_playing');
-    
-    final List<Movie> movies = [];
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDbResponse.results
+    .where((movieDB) => movieDB.posterPath != 'no-poster')
+    .map(
+      (movieDB) => MovieMapper.movieDbToEntitie(movieDB)
+    ).toList();
 
     return movies;
   }
