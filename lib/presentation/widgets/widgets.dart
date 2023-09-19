9a3918f2dc6a1a087c039db95c3eb7e4cba72c95
MATEO JAVIER AGUILAR CARRILLO <mateo.aguilar@elektra.com.mx>
2023-09-19 14:16:36 -0600

    initial loader provider added

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,5 +1,9 @@
 
+export 'package:cinemapedia/presentation/widgets/shared/full_screen_loader.dart';
+
 export 'package:cinemapedia/presentation/widgets/movies/movie_horizontal_listview.dart';
 export 'package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart';
 export 'shared/custom_appbar.dart';
 export 'shared/custom_bottom_navigationbar.dart';
+
+
