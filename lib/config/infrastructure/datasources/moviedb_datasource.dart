28d8e7c721e4edf31065b5008810c458e66a18b4
MATEO JAVIER AGUILAR CARRILLO <mateo.aguilar@elektra.com.mx>
2023-09-19 21:17:18 -0600

    datasources, repositories and mapper added for movie_screen

--- lib/config/infrastructure/datasources/moviedb_datasource.dart
+++ lib/config/infrastructure/datasources/moviedb_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/config/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/config/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/config/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -49,5 +50,15 @@ class MoviedbDataSource extends MoviesDatasource {
     final response = await dio.get('/movie/top_rated', queryParameters: { 'page': page });
     return _jsonToMovies(response.data);
   }
+  
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get('/movie/${id}');
+    if(response.statusCode != 200 ) throw Exception('Movie with: $id not found');
 
+    final movieDetails = MovieDetails.fromJson(response.data);
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+
+    return movie;
+  }
 }
--- lib/config/infrastructure/mappers/movie_mapper.dart
+++ lib/config/infrastructure/mappers/movie_mapper.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/config/infrastructure/models/moviedb/movie_from_moviedb.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -20,5 +21,27 @@ class MovieMapper {
       title: movieDB.title,
       video: movieDB.video,
       voteAverage: movieDB.voteAverage,
-      voteCount: movieDB.voteCount);
+      voteCount: movieDB.voteCount
+    );
+
+    static Movie movieDetailsToEntity(MovieDetails movieDB) => Movie(
+      adult: movieDB.adult,
+      backdropPath: movieDB.backdropPath != '' 
+        ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+        : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+      genreIds: movieDB.genres.map((e) => e.name).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: movieDB.posterPath != ''
+        ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+        : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount
+    );
 }
